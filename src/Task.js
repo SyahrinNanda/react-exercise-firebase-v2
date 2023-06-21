1fafed4d342ff210ac6482743f9eda0df9574571
SyahrinNanda <syahrinnanda@gmail.com>
2023-06-22 01:47:43 +0800

    add file

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://react-exercise-firebase-72be8.web.app/"; // TODO: replace this
+const studentName = "As'syahrin Nanda"; // TODO: replace this
+const studentId = "FE5130073"; // TODO: replace this
+
+export { url, studentName, studentId };
